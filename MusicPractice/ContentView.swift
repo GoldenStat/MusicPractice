27ccb0d99fca1e07bd8b10f981fe6a5383a61232
Alexander Voelz <alexander.voelz@de.ibm.com>
2020-04-22 12:46:43 +0200

    Initial Commit

---
+++ MusicPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPractice
+//
+//  Created by Alexander Völz on 22.04.20.
+//  Copyright © 2020 Alexander Völz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
