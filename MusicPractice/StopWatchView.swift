5146feca25330ca6df4320785bf9615ebd6ebf4d
Alexander Voelz <alexander.voelz@de.ibm.com>
2020-05-01 15:53:21 +0200

    stopWatchDesign

--- MusicPractice/StopWatchView.swift
+++ MusicPractice/StopWatchView.swift
@@ -19,19 +19,7 @@ struct StopWatchView: View {
     var isRunning: Bool { stopWatch.isRunning }
     var time: String { stopWatch.counter.string }
     
-    enum ClockState : String {
-        case pause, start, lap, reset
-    }
-    
-    var colors : [ClockState:Color] = [
-        .pause: .red,
-        .start: .green,
-        .lap: .yellow,
-        .reset: .orange,
-    ]
-    
-    var pauseResetState: ClockState { isRunning ? .lap : .reset }
-    var stopStartState: ClockState { isRunning ? .pause : .start }
+    var wrappedString = (on: "Start", off: "Pause")
     
     var body: some View {
         VStack {
@@ -40,17 +28,26 @@ struct StopWatchView: View {
             VStack {
                 Text(time)
                     .font(.title)
-                
+        
                 HStack {
-                    Button(stopStartState.rawValue.capitalized) { // start / stop button
-                        self.stopWatch.toggleStopStart()
-                    }
-                    .buttonStyle(ScaleButtonStyle(color: colors[stopStartState]!))
                     
-                    Button(pauseResetState.rawValue.capitalized) { // start / stop button
-                        self.stopWatch.toggleLapReset()
+                    Button( isRunning ? wrappedString.off : wrappedString.off ) {
+                      self.stopWatch.toggleStopStart()
                     }
-                    .buttonStyle(ScaleButtonStyle(color: colors[pauseResetState]!))
+                    .buttonStyle(StopWatchButtonStyle(
+                        color: (on: .green, off: .red),
+                        isRunning: isRunning))
+                    
+                    StopWatchButton(stopWatch: stopWatch,
+                                    string: (on: "Start", off: "Pause"),
+                                    color: (on: .green, off: .red))
+                    { self.stopWatch.toggleStopStart() }
+                    
+                    StopWatchButton(stopWatch: stopWatch,
+                                    string: (on: "Reset", off: "Lap"),
+                                    color: (on: .orange, off: .yellow))
+                    { self.stopWatch.toggleLapReset() }
+                    
                 }
             }
             .padding()
@@ -59,10 +56,57 @@ struct StopWatchView: View {
     
 }
 
+struct StopWatchButton: View {
+
+    @ObservedObject var stopWatch : StopWatch
+
+    let string: (on: String, off: String)
+    let color: (on: Color, off: Color)
+
+    var isRunning: Bool { stopWatch.isRunning }
+
+    let action : () -> ()
+
+    var wrappedString: String { isRunning ? string.off : string.on }
+    var wrappedColor: Color { isRunning ? color.off : color.on }
+
+    var body: some View {
+        Button(wrappedString.capitalized) {
+            self.action()
+        }
+        .buttonStyle(ScaleButtonStyle(color: wrappedColor))
+    }
+
+}
+
+
+struct StopWatchButtonStyle: ButtonStyle {
+    
+    let color: (on: Color, off: Color)
+    
+    var isRunning: Bool
+
+    var wrappedColor: Color { isRunning ? color.off : color.on }
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Circle()
+            .fill(wrappedColor)
+            .overlay(
+                Circle()
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.white)
+                    .padding(4)
+        )
+            .overlay(configuration.label
+                .foregroundColor(.white)
+        )
+            .frame(maxWidth: 75, maxHeight: 75)
+    }
+}
 
 struct ScaleButtonStyle: ButtonStyle {
     
-    var color: Color
+    let color: Color
     
     func makeBody(configuration: Self.Configuration) -> some View {
         Circle()
